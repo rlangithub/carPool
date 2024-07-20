976864f379431699c003ca5b38477a506cbdbc33
rlanggithub <r0556721183@gmail.com>
2024-07-20 22:11:33 +0300

    start run

--- services/jwt.js
+++ services/jwt.js
@@ -5,8 +5,6 @@ exports.generateToken =(driverID, driverPassword)=> {
         ID: driverID,
         password: driverPassword
     };
-
-    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '2m' });
-    
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '10m' });
     return token;
 }
